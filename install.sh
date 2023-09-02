1d040e2b209e0a1aa44c76cdeca8297217026652
ss108 <samier_saeed@protonmail.com>
2023-09-01 21:31:57 -0700

    install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+DOTFILES_DIR="`pwd`"
+
+target_dir=$HOME
+
+files=(".bash_profile", ".bash_aliases", ".bashrc", ".zshrc")
+
+# Loop through the list of files and create symbolic links
+for file_name in "${files[@]}"; do
+    source_file="${DOTFILES_DIR}/${file_name}"
+    symlink="${target_dir}/${file_name}"
+    
+    ln -sfv "$source_file" "$symlink"
+done
+
+
+
